4a0b73773785c72351c0dc82f712372d2a86d8d4
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2024-02-14 04:41:01 +0400

    fix: change the theme depending on system status

--- app/theme-switcher.tsx
+++ app/theme-switcher.tsx
@@ -1,10 +1,17 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { useEffect } from 'react'
 
 const ThemeSwitcher = () => {
+    const isDark = window.matchMedia('(prefers-color-scheme:dark)').matches
     const { resolvedTheme: theme, setTheme } = useTheme()
     const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark')
+
+    useEffect(() => {
+        setTheme(isDark ? 'dark' : 'light')
+    }, [isDark, setTheme])
+
     return (
         <button
             type='button'
